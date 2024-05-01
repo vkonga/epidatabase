4ff69c3104e3c4fc900c65ac40aab74ee9e895b2
venkatesh kongara <vkongara048@gmail.com>
2024-05-01 19:57:32 +0530

    first commit

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 const express = require('express')
-const app = express()
 const {open} = require('sqlite')
 const sqlite3 = require('sqlite3')
 const path = require('path')
-app.use(express.json())
 
 const dbPath = path.join(__dirname, 'tasksdb.db')
-
+const app = express()
+app.use(express.json())
 let db = null
 
 const initializeDBAndServer = async () => {
@@ -15,11 +14,12 @@ const initializeDBAndServer = async () => {
       filename: dbPath,
       driver: sqlite3.Database,
     })
+
     app.listen(3000, () => {
-      console.log('Server Running at localhost://3000')
+      console.log("Server is running at server 'http://localhost:3000/")
     })
-  } catch (e) {
-    console.log(`DB Error: ${e.message}`)
+  } catch (error) {
+    console.log(`DB Error: ${error.message}`)
     process.exit(1)
   }
 }
@@ -28,82 +28,92 @@ initializeDBAndServer()
 
 app.get('/tasks/', async (request, response) => {
   const getTaskQuery = `
-        SELECT *
-        FROM tasks
-        `
-  const taskQuery = await db.all(getTaskQuery)
-  response.send(taskQuery)
+    SELECT * FROM tasks;
+  `
+
+  const tasksQuery = await db.all(getTaskQuery)
+  response.send(tasksQuery)
 })
 
-app.get('/tasks/:id/', async (request, response) => {
-  const {id} = request.params
-  const getTasksQuery = `
-    SELECT *
-    FROM tasks
-    WHERE
-      id = ${id};
+app.get('/tasks/:taskId', async (request, response) => {
+  const {taskId} = request.params
+  const getTaskQuery = `
+    SELECT * FROM tasks
+    WHERE task_id = ${taskId};
   `
-  const taskQuery = await db.get(getTasksQuery)
-  response.send(taskQuery)
+  const tasksQuery = await db.get(getTaskQuery)
+  response.send(tasksQuery)
+})
+
+app.get('/tasks/:taskId/eachtasks/', async (request, response) => {
+  const {taskId} = request.params
+  const getTaskQuery = `
+    SELECT * FROM eachtasks
+    WHERE task_id = '${taskId}';
+  `
+
+  const tasksQuery = await db.all(getTaskQuery)
+  response.send(tasksQuery)
 })
 
 app.post('/tasks/', async (request, response) => {
-  const {user,taskName ,taskDescription,taskStatus,taskDue} = request.body;
-
-  const getTaskQuery = `
+  const {user} = request.body
+  const addTaskQuery = `
     INSERT INTO 
-      district (user,task_name,task_description,task_status,task_due)
-    VALUES
-      (
-        
-        '${user}',
-        '${taskName}',
-        '${taskDescription}',
-        '${taskStatus}',
-        '${taskDue}'
-      );
-
+      tasks(user)
+    VALUES ('${user}');
   `
 
-  const taskQuery = await db.run(getTaskQuery)
-
-  response.send('District Successfully Added')
+  const addResponse = await db.run(addTaskQuery)
+  const tasksId = addResponse.lastID
+  response.send('Successfully added')
 })
 
+app.put('/tasks/:taskId', async (request, response) => {
+  const {user} = request.body
+  const {taskId} = request.params
+  const updateTaskQuery = `
+    UPDATE tasks
+    SET user ='${user}'
+    WHERE task_id = ${taskId};
+  `
+  await db.run(updateTaskQuery)
+  response.send('succefully updated')
+})
 
-app.put('/tasks/:id/', async (request, response) => {
-  const {id} = request.params
+app.post('/tasks/:taskId/eachtasks/', async (request, response) => {
+  const {taskName, taskDescription, taskStatus, taskDue} = request.body
+  const {taskId} = request.params
+  const addTasksQuery = `
+    INSERT INTO 
+      eachtasks(task_name,task_description,task_status,task_due,task_id)
+      VALUES (
+        '${taskName}','${taskDescription}','${taskStatus}','${taskDue}',${taskId}
+      )
+      ;
+  `
+  const addResponse = await db.run(addTasksQuery)
+  const tasksId = addResponse.lastID
+  response.send('successfully added')
+})
 
-  const {user,taskName ,taskDescription,taskStatus,taskDue} = request.body
-
-  const taskQuery = `
-    UPDATE 
-      tasks
-    SET 
-      user = '${user}',
+app.put('/tasks/:taskId/eachtasks/:id', async (request, response) => {
+  const {id, taskId} = request.params
+  const {taskName, taskDescription, taskStatus, taskDue} = request.body
+  const updateTaskQuery = `
+    UPDATE eachtasks
+    SET
       task_name = '${taskName}',
       task_description = '${taskDescription}',
       task_status = '${taskStatus}',
-      task_due = '${taskDue}'
+      task_due = '${taskDue}',
+      task_id = '${taskId}'
     WHERE 
       id = ${id};
-  `;
+  `
 
-  await db.run(taskQuery)
-  response.send('District Details Updated')
+  await db.run(updateTaskQuery)
+  response.send('successfully updated')
 })
 
-
-
-app.delete('/tasks/:id/', async (request, response) => {
-    const {id} = request.params
-  
-    const deleteTaskQuery = `
-      DELETE FROM tasks
-      WHERE id = ${id};
-    `
-  
-    await db.run(deleteTaskQuery)
-    response.send('District Removed')
-  })
-  module.exports = app
+module.exports = app
